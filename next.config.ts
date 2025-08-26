03f392be345c7f92c7b78f5c06d4dc1e7a93acf4
Shahrad Meyghani <shahradmeyghani@Shahrads-MacBook-Pro.local>
2025-08-26 19:14:41 +0100

    fix: remove duplicate next-env and relax eslint on builds

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
